d80f615fd30cbe8ba8a04df5e5a7c591b1e7956b
nonepop <bykovdanyl2021@gmail.com>
2026-05-23 19:44:09 +0500

    Update ping_pong.py
    добавил мячу движение и условия проигрыша и отталкивания

--- ping_pong.py
+++ ping_pong.py
@@ -47,6 +47,13 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 back = (200,255,255)
 window.fill(back)
+font.init()
+font = font.SysFont('Arial', 36)
+lose1 = font.render("Player 1 lose", True, (255,0,0))
+lose2 = font.render("Player 2 lose", True, (255,0,0))
+
+
+
 
 
 
@@ -55,7 +62,8 @@ player2 = Player("raketka.png", win_width-150, 100, 10, 100, 130)
 machik = GameSprite("machik.png", 350,250,7,40,40)
 
 
-
+speed_x = 3
+speed_y = 3
 
 finish = False
 game = True
@@ -75,7 +83,24 @@ while game:
         player2.update_r()
         player2.reset()
         machik.reset()
-        
+        machik.rect.x += speed_x
+        machik.rect.y += speed_y
+
+
+
+    
+        if sprite.collide_rect(player1, machik) or sprite.collide_rect(player2, machik):
+            speed_x *= -1
+
+        if machik.rect.y >= 480 or machik.rect.y <= 0:
+            speed_y *= -1
+
+
+        if machik.rect.x >= 695:
+            window.blit(lose2,(200,200))
+
+        if machik.rect.x <= 5:
+            window.blit(lose1,(200,200))
 
 
 
